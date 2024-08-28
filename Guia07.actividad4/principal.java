01e96d9edae72fcc45d17674c20c90452d6ef700
Cardenas Trujillo Alonso <hancel.cardenas.trujillo@vallegrande.edu.pe>
2024-08-28 18:45:52 -0500

    Add files via upload

---
+++ Guia07.actividad4/principal.java
@@ -0,0 +1,20 @@
+package Guia07.actividad4;
+
+import javax.swing.JOptionPane;
+
+public class principal {
+
+    public static void main(String[] args) {
+                
+        calcular op = new calcular();
+        
+        int A = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número: "));
+        int B = Integer.parseInt(JOptionPane.showInputDialog("Ingrese otro número: "));
+        
+        op.suma(A,B);
+        op.resta(A,B);
+        op.mult(A,B);
+        op.mostrarDatos();
+    }
+    
+}
